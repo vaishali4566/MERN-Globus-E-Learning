a003b22f46e5474ef4f7d3124d3d91415cfa6cf3
vaishali4566 <vaishaliverma@globussoft.in>
2025-12-30 13:45:54 +0530

    created login and signup api

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 
-// import loadRoutes from "./routes/index.js";
-// import { errorHandler } from "./middlewares/error.middleware.js";
+import loadRoutes from "./routes/index.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -20,9 +20,9 @@ app.get("/health", (req, res) => {
 });
 
 /* ---------- Routes ---------- */
-// loadRoutes(app);
+loadRoutes(app);
 
 /* ---------- Error Handler (ALWAYS LAST) ---------- */
-// app.use(errorHandler);
+app.use(errorHandler);
 
 export default app;
---
+++ server/src/modules/auth/auth.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true, minlength: 6 },
+  role: { 
+    type: String, 
+    enum: ["student", "trainer", "admin"], 
+    default: "student" 
+  },
+
+  // Trainer-specific fields
+  bio: { type: String },
+  skills: { type: [String], default: [] },
+
+  // Admin-specific fields
+  permissions: { type: [String], default: [] }, 
+}, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+
+// Compare password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+}
+
+export default mongoose.model("User", userSchema);
