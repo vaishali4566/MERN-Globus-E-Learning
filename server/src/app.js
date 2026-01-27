08dd8d9e378e054a24d1f275d60a77382da17377
vaishali4566 <vaishaliverma@globussoft.in>
2026-01-27 10:42:04 +0530

    fixed cors issue

--- server/src/app.js
+++ server/src/app.js
@@ -9,8 +9,7 @@ const app = express();
 /* ---------- Global Middlewares ---------- */
 app.use(
   cors({
-    origin: "http://localhost:5173",  
-    
+    origin: "http://localhost:5173",
     credentials: true,               
   })
 );
