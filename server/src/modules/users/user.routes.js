0ff2b9abb42c14fbc9d7b71a732e69038a5a551d
vishalglobussoftSDE <vishalvishwakarma@globussoft.in>
2026-01-29 12:17:08 +0530

    yaa yaa yaa updated user controller for edit profile

--- server/src/modules/users/user.routes.js
+++ server/src/modules/users/user.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getProfile } from "./user.controller.js";
+import { getProfile ,editProfile} from "./user.controller.js";
 import { protect } from "../../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/profile", protect, getProfile);
+router.put("/profile", protect, editProfile);
 
 export default router;
